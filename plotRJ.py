c249626c232d775fbf9438b301628bc3a81bea5f
Bernard  J Koch <akathriel@gmail.com>
2018-07-08 19:45:16 -0700

    Major revision to plotRJ. Using seaborn to print out most plots.

--- plotRJ.py
+++ plotRJ.py
@@ -7,6 +7,8 @@ import csv
 import argparse
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 
 def calcHPD(data, level=0.95) :
 	assert (0 < level < 1)	
@@ -212,10 +214,53 @@ def get_K_values(mcmc_tbl,head,col,par,burnin=0.2):
 def get_trait_values(mcmc_tbl,head,par,burnin=0.2):
 	burnin=min(int(burnin*len(mcmc_tbl)),int(0.9*len(mcmc_tbl)))
 	post_tbl = mcmc_tbl[burnin:,:]
+
 	if par=="l":
 		return np.mean(post_tbl[:,[head.index("x0_l"),head.index('kappa_l')]],axis=0)
+		
 	else:
 		return np.mean(post_tbl[:,[head.index("x0_m"),head.index('kappa_m')]],axis=0)
+
+def plot_trait_params(mcmc_tbl,head,axes,burnin=0.2):
+	burnin=min(int(burnin*len(mcmc_tbl)),int(0.9*len(mcmc_tbl)))
+	post_tbl = mcmc_tbl[burnin:,:]
+	ax1,ax2,ax3,ax4=axes
+	sns.distplot(post_tbl[:,head.index("x0_l")],ax=ax1,color='blue')
+	sns.distplot(post_tbl[:,head.index("kappa_l")],ax=ax2,color='blue')
+	sns.distplot(post_tbl[:,head.index("x0_m")],ax=ax3,color='red')
+	sns.distplot(post_tbl[:,head.index("kappa_m")],ax=ax4,color='red')
+
+
+	ax1.set_title('x0')
+	ax2.set_title('kappa')
+	ax1.set_ylabel('Birth')
+	ax3.set_ylabel('Death')
+	
+def plot_rate_ribbons(b_res,d_res,b_ax=None,d_ax=None,logT=-1):
+	glbl_min=np.nanmin([b_res[2],d_res[2]])
+	glbl_max=np.nanmax([b_res[3],d_res[3]])
+	
+	b_ax.set_ylim(glbl_min,glbl_max)
+	d_ax.set_ylim(glbl_min,glbl_max)
+	b_ax.set_xticks(b_res[0][::5])
+	d_ax.set_xticks(b_res[0][::5])
+	plt.setp(b_ax.get_xticklabels(), rotation=45)
+	plt.setp(d_ax.get_xticklabels(), rotation=45)
+	
+	b_ax.plot(b_res[0],b_res[1][::-1], lw=2, color='blue')
+	b_ax.fill_between(b_res[0], b_res[2][::-1], b_res[3][::-1], facecolor='blue', alpha=0.5)              
+	b_ax.set_xlabel('Year')
+	
+	d_ax.plot(d_res[0],d_res[1][::-1], lw=2, color='red')
+	d_ax.fill_between(d_res[0], d_res[2][::-1], d_res[3][::-1], facecolor='red', alpha=0.5)              
+	d_ax.set_xlabel('Year')
+	if logT==1:
+		b_ax.set_yscale('log',basey=10)
+		b_ax.set_title('log10 Rates')
+		d_ax.set_yscale('log',basey=10)
+	else:
+		b_ax.set_title('Rates')
+	
 
 #helper for calculate trait rates
 def transform_rate_logistic(r0,x0,kappa,trait):
@@ -224,53 +269,70 @@ def transform_rate_logistic(r0,x0,kappa,trait):
 	return rate_at_trait
 
 def calculate_trait_rates(trait_profile,base_rate, x0, kappa):
-	trait_rates=pd.Series(trait_profile.index).apply(lambda tval: transform_rate_logistic(base_rate,x0,kappa,float(tval) ) if trait_profile[tval]!=0 else None ) #note that tval.name is a trait value
+	#Here trait profile is a array of counts of species for different trait values in a SPECIFIC year
+	#float(tval) is therefore the value of the actual trait
+	#trait_profile[tval] is the number of bands, so if there are no bands just return none
+	trait_rates=pd.Series(trait_profile.index).apply(lambda tval: transform_rate_logistic(base_rate,x0,kappa,float(tval) ) if trait_profile[tval]!=0 else None ) 
 	return trait_rates
 
 #Create two tables: trait x year x abundance & trait x year x over baseline	
 def make_trait_value_tables(trait_file,trait_type,res,par,x0,kappa):
-	trait_file='/home/bernie/Documents/Github/LiteRate/example_dataTAD.age.txt'
-	trait_df=pd.  read_csv(trait_file,sep='\t',index_col=False).iloc[:,4:] # Skip clade, species, ts, te
-	trait_type='int'
+	trait_df=pd.read_csv(trait_file,sep='\t',index_col=False).iloc[:,4:] # Skip clade, species, ts, te
 	if trait_type=='int':
 		#get range of trait vals
 		t_min=trait_df.min().min().astype(int)
 		t_max=trait_df.max().max().astype(int)
-		t_bins=np.array(range(t_min,t_max+1))+.5 #I dont think the .5 is necessary but I'm doing it for consistency
-		t_year_hist=trait_df.apply(lambda clm: np.histogram(clm.dropna(),t_bins)[0],axis=0) #get count of values by year
-
-		base_rates=pd.Series(res[1])
-		base_rates.index=trait_df.columns[:-1]
+		t_bins=np.array(range(t_min,t_max+1))+.5 #I dont think the .5 is necessary but I'm doing it for consistency so this looks like time_frames
+		
+		#create a dataframe trait value X year filled with number of species alive
+		t_year_hist=trait_df.apply(lambda clm: np.histogram(a=clm.dropna(),bins=t_bins)[0],axis=0)
+		base_rates=pd.Series(res[1][::-1]) #WHY IS THIS REVERSED
+		base_rates.index=trait_df.columns[:-1] #WHY do this Daniele (looking at time_frames)?
+		#see calculate_trait_rates to understand whats going on here, but result is  
 		rate_df=pd.Series(base_rates.index).apply(lambda year:calculate_trait_rates(t_year_hist.loc[:,year],base_rates[year],x0,kappa)) #note that rate.name is a year
-		rate_df.index=trait_df.columns[:-1]
+		rate_df.index=trait_df.columns[:-1] #WHY do this Daniele (looking at time_frames)?
+		
+		
 		rate_df.index.rename('year',inplace=True)
-		#mega_df.reset_index(inplace=True)
-#		mega_df=mega_df.melt(id_vars=['year'])
-#		mega_df.columns=['year','trait','rate']
-#		mega_df.to_csv('/home/bernie/Documents/Github/LiteRate/example_dataTAD.age.heatmap.{}.csv'.format(par),sep='\t',index=False)		
+		rate_df['ALL']=base_rates
+		rate_df=rate_df.transpose()
+		T=t_year_hist.sum()
+		T.name="ALL"
+		t_year_hist=t_year_hist.append(T)
 		return t_year_hist, rate_df
 
 #plot birth rate over BL, abundance, death rate over BL
-def make_seaborn_heatmaps(trait_abundance,trait_birth_tbl,trait_death_tbl):
+def make_seaborn_heatmaps(trait_abundance,trait_birth_tbl,trait_death_tbl,logT=-1,plot_title='default'):
+	fig, (ax1, ax2, ax3) = plt.subplots(ncols=3)
+	fig.suptitle(plot_title,y=1.02)
+	if logT==1:
+		trait_abundance=trait_abundance.transform(lambda x: np.log10(x+1))
+		trait_birth_tbl=trait_birth_tbl.transform(lambda x: np.log10(x+1))
+		trait_death_tbl=trait_death_tbl.transform(lambda x: np.log10(x+1))
+
 	glbl_min=min(trait_birth_tbl.min().min(),trait_death_tbl.min().min())
 	glbl_max=max(trait_birth_tbl.max().max(),trait_death_tbl.max().max())
-	fig, (ax1, ax2, ax3) = plt.subplots(ncols=3)
-	sns.heatmap(trait_birth_tbl.transpose(),ax=ax1,cmap=sns.cm.icefire,center=0,vmin=glbl_min,vmax=glbl_max)
+	
+	
+	sns.heatmap(trait_birth_tbl,ax=ax1,cmap=sns.cm.icefire,center=0,vmin=glbl_min,vmax=glbl_max)
 	sns.heatmap(trait_abundance,ax=ax2,cmap=sns.cm.rocket_r,vmin=0)
-	sns.heatmap(trait_death_tbl.transpose(),ax=ax3,cmap=sns.cm.icefire,center=0,vmin=glbl_min,vmax=glbl_max)
+	sns.heatmap(trait_death_tbl,ax=ax3,cmap=sns.cm.icefire,center=0,vmin=glbl_min,vmax=glbl_max)
 	ax1.set_title("Birth R. (over BL)")
 	ax2.set_title("Abundance")
 	ax3.set_title("Death R. (over BL)")
-	#ax1.set_xlabel("Trait Value")
 	ax2.set_xlabel("Year")
 	ax1.set_ylabel("Trait Value")
 	ax1.xaxis.label.set_visible(False); ax3.xaxis.label.set_visible(False)
 	ax1.invert_yaxis(); ax2.invert_yaxis(); ax3.invert_yaxis()
 	plt.tight_layout()
+	if logT==1:
+		ax2.set_xlabel("Year (ALL LOG10 SCALE)")
+	return fig
+
+#def make_seaborn_rate_ribbons
 
 
-
-def plot_marginal_rates(path_dir,trait_file,name_tag="",bin_size=1.,burnin=0.2,min_age=0,max_age=0,logT=0):
+def plot_marginal_rates(path_dir,trait_dir,name_tag="",bin_size=1.,burnin=0.2,min_age=0,max_age=0,logT=0):
 	direct="%s/*%s*mcmc.log" % (path_dir,name_tag)
 	files=glob.glob(direct)
 	files=np.sort(files)
@@ -282,13 +344,13 @@ def plot_marginal_rates(path_dir,trait_file,name_tag="",bin_size=1.,burnin=0.2,m
 	else: outname = ""
 	if max_age>0: outname+= "t%s" % (int(max_age))
 	r_str = "\n\npdf(file='%s/%sRTT_plots.pdf',width=12, height=8)\npar(mfrow=c(2,3))\nlibrary(scales)" % (wd,outname)
+	trait_out_pdf=PdfPages(wd+'/'+outname+'TRAITplots.pdf')
 	for mcmc_file in files:
 		if 2>1: #try:
 			name_file = os.path.splitext(os.path.basename(mcmc_file))[0]		
 			if min_age==0 and max_age==0: # get empirical time range
 				tbl=np.loadtxt(mcmc_file, skiprows=1)
 				head = next(open(mcmc_file)).split() # should be faster
-				print("HEAD", head)                
 				if present_year == -1:
 					max_age_t = np.mean(tbl[:,head.index("root_age")])
 					min_age_t = np.mean(tbl[:,head.index("death_age")])
@@ -297,34 +359,47 @@ def plot_marginal_rates(path_dir,trait_file,name_tag="",bin_size=1.,burnin=0.2,m
 					min_age_t = present_year-np.mean(tbl[:,head.index("death_age")])
 			else:
 				min_age_t, max_age_t = min_age, max_age
-			print("AGE IS JUST A NUMBER",min_age_t,max_age_t)
 			nbins = int(abs(max_age_t-min_age_t)/float(bin_size))
 			colors = ["#4c4cec","#e34a33"] # sp and ex rate
 			# sp file
 			r_str += get_K_values(tbl,head,colors[0],"l",burnin=0.2)
 			f_name = mcmc_file.replace("mcmc.log","sp_rates.log")
 
-			res = get_marginal_rates(f_name,min_age_t,max_age_t,nbins,burnin=0.2)
-			
+			b_res = get_marginal_rates(f_name,min_age_t,max_age_t,nbins,burnin=0.2)
 			#trait rate commands
-			x0_l,kappa_l=get_trait_values(tbl,head,'l',burnin=.2)
-			abundance_df, bt_df=make_trait_value_tables(trait_file,'int',res,'birth',x0_l,kappa_l)
 			
 			
-			r_str += get_r_plot(res,col=colors[0],parameter="Speciation rate",min_age=min_age_t,max_age=max_age_t,plot_title=name_file,plot_log=logT)
+			r_str += get_r_plot(b_res,col=colors[0],parameter="Speciation rate",min_age=min_age_t,max_age=max_age_t,plot_title=name_file,plot_log=logT)
 			# ex file
 			r_str += get_K_values(tbl,head,colors[1],"m",burnin=0.2)
 			f_name = mcmc_file.replace("mcmc.log","ex_rates.log")
-			res = get_marginal_rates(f_name,min_age_t,max_age_t,nbins,burnin=0.2)
+			d_res = get_marginal_rates(f_name,min_age_t,max_age_t,nbins,burnin=0.2)
+			
+			r_str += get_r_plot(d_res,col=colors[1],parameter="Extinction rate",min_age=min_age_t,max_age=max_age_t,plot_title="",plot_log=logT,run_simulation=0)
 			
 			#trait rate commands
-			x0_m,kappa_m=get_trait_values(tbl,head,'m',burnin=.2)
-			abundance_df,dt_df=make_trait_value_tables(trait_file,'int',res,'death',x0_m,kappa_m)
-			make_seaborn_heatmaps(abundance_df,bt_df,dt_df)
-
-			r_str += get_r_plot(res,col=colors[1],parameter="Extinction rate",min_age=min_age_t,max_age=max_age_t,plot_title="",plot_log=logT,run_simulation=0)
+			if 'x0_l' in head:
+				print(name_file)
+				trait_file=trait_dir+name_file.replace("_trait_mcmc",".txt")
+				print(trait_file)
+				fig, ((ax1, ax2 ,ax3), (ax4,ax5, ax6)) = plt.subplots(ncols=3,nrows=2)
+				fig.suptitle(name_file,y=1.02)
+				plot_trait_params(tbl,head,axes=[ax1,ax2,ax4,ax5],burnin=.2)
+				plot_rate_ribbons(b_res,d_res,ax3,ax6)
+				trait_out_pdf.savefig(fig, bbox_inches='tight')
+				
+				x0_l,kappa_l=get_trait_values(tbl,head,'l',burnin=.2)
+				abundance_df, bt_df=make_trait_value_tables(trait_file,'int',b_res,'birth',x0_l,kappa_l)
+				
+				x0_m,kappa_m=get_trait_values(tbl,head,'m',burnin=.2)
+				abundance_df,dt_df=make_trait_value_tables(trait_file,'int',d_res,'death',x0_m,kappa_m)
+				
+				map_fig=make_seaborn_heatmaps(abundance_df,bt_df,dt_df,plot_title=name_file)
+				trait_out_pdf.savefig(map_fig, bbox_inches='tight')
+			
 		#except:
 		#	print "Could not read file:", mcmc_file
+	trait_out_pdf.close()
 	r_str += "\n\nn <- dev.off()"
 	out="%s/%sRTT_plots.r" % (wd,outname)
 	outfile = open(out, "wb") 
@@ -332,7 +407,7 @@ def plot_marginal_rates(path_dir,trait_file,name_tag="",bin_size=1.,burnin=0.2,m
 	outfile.close()
 	cmd="cd %s; Rscript %sRTT_plots.r" % (wd,outname)
 	print "Plots saved in %s (%sRTT_plots)" % (wd,outname)
-	os.system(cmd)
+	#os.system(cmd)
 
 
 
@@ -341,12 +416,10 @@ p = argparse.ArgumentParser() #description='<input file>')
 p.add_argument('input_data', metavar='<path to log files>', type=str,help='Input python file - see template',default="")
 p.add_argument('-logT', metavar='1', type=int,help='set to 1 to log transform rates',default=0)
 p.add_argument('-present_year',    type=int, help='set to > present AD to plot in time AD instead of time BP', default= -1, metavar= -1)
-#p.add_argument('-trait_data', metavar='<path to traitrate input>', type=str,help='Lite Trait Rate Input File',default="")
-#p.add_argument('-trait_dtype', metavar='trait_type', type=str,help='Lite Trait Rate Input File',default="int", choices=['int','float'])
+p.add_argument('-trait_dir', metavar='<path to traitrate input>', type=str,help='Lite Trait Rate Input File if You Have Trait Rate',default="")
+p.add_argument('-trait_dtype', metavar='trait_type', type=str,help='Lite Trait Rate Input File',default="int", choices=['int','float'])
 
-#args = p.parse_args()
-#path_dir_log_files = args.input_data
-path_dir_log_files='/home/bernie/Documents/Github/LiteRate/pyrate_mcmc_logs'
-#present_year = args.present_year
-present_year=2018
-plot_marginal_rates(trait_file='/home/bernie/Documents/Github/LiteRate/example_dataTAD.age.txt',logT=1,path_dir=path_dir_log_files)
+args = p.parse_args()
+path_dir_log_files = args.input_data
+present_year = args.present_year
+plot_marginal_rates(trait_dir=args.trait_dir,logT=args.logT,path_dir=path_dir_log_files)
